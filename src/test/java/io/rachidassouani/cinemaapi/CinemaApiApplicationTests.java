91cf30bb85b32e3c84d9c1964b5a137d8de423fb
rachidassouani <rachidassouani@gmail.com>
2020-05-29 19:20:16 +0000

    init project

---
+++ src/test/java/io/rachidassouani/cinemaapi/CinemaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rachidassouani.cinemaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
